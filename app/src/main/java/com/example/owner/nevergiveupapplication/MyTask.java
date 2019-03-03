3ddf2cbf1a645684c613d14333eae45e80774751
Owner <byanmiary1@gmail.com>
2019-03-03 12:21:19 +0200

    aaa

---
+++ app/src/main/java/com/example/owner/nevergiveupapplication/MyTask.java
@@ -0,0 +1,9 @@
+package com.example.owner.nevergiveupapplication;
+
+public class MyTask {
+    public int getImportant;
+    public int getNecessary;
+
+    public int getText() {return 0;}
+    public int getTitle(){return 0;}
+}
